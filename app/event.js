14b0f62543be72bc61c61a6ea1ed8b2f8af56a5a
GithuiVictor <victormaina23@gmail.com>
2020-06-06 19:47:34 +0300

    App

--- app/event.js
+++ app/event.js
@@ -83,26 +83,25 @@ function initMap() {
 const eventCard = (event) => {
     const eventTime = `2020-01-01 ${event.eventTime}`;
     return `
-     <div class="col-md-4" data-aos="fade-right">
-        <div class="card card-1">
+    <div class="col-md-4" data-aos="fade-left">
+        <div class="card card-3">
             <img src="${event.image}" alt="img1" class="card-img-top">
             <div class="card-body">
                 <h4 class="card-title">Event</h4>
                 <p class="card-text">
-                    <i class="fas fa-calendar-week"></i> ${moment(event.eventDate).format('MMMM Do YYYY')}
+                    <i class="fas fa-calendar-week"></i> ${moment(event.eventDate).format('MMMM Do YYYY')} 
                     <i class="fas fa-clock ml-2"></i> ${moment(event.eventTime).format('h:mm a')}
-                    <br>
+                    <br> 
                     <i class="fas fa-map-marker-alt"></i> Venue
-                    <i class="fas fa-ticket-alt ml-2"></i> <span>${event.paid && event.amount || 'free'}</span>
+                    <i class="fas fa-ticket-alt ml-2"></i> Kshs <span>${event.paid && event.amount || 'free'}</span>
                 </p>
                 <label for="" class="card-text details">
                     ${event.shortDescription}
                 </label>
-                <div class="card-footer mt-3">
-                    <a href="eventDetails.html?event=${event.id}">
-                        <input type="button" value="RSVP" class="btn">
-                    </a>
-                </div>
+                <br>
+                <a href="eventDetails.html?event=${event.id}">
+                    <input type="button" value="RSVP" class="btn">
+                </a>
             </div>
         </div>
     </div>
